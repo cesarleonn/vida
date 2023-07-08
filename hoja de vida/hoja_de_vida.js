2cbdf08763d1d7545f0dc38ad92da25dc095e6e5
cesarleonn <124885780+cesarleonn@users.noreply.github.com>
2023-07-08 00:51:17 -0500

    hola
    hoja de vida

---
+++ hoja de vida/hoja_de_vida.js
@@ -0,0 +1,5 @@
+document.getElementById("btnPasajes").addEventListener("click", function() {
+  
+ 
+    window.location.href = "https://drive.google.com/drive/folders/1VN_huED-drQzYtu8Ge4QtK5yYtPUKVvc?usp=sharing";
+    });
